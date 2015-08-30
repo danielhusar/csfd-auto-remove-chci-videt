9930d9dae53151439410735e7fb3bdfae57ea39c
Daniel Husar <dano.husar@gmail.com>
2015-08-30 21:06:00 +0100

    small refactor

--- index.js
+++ index.js
@@ -6,15 +6,19 @@
   $ratingForm.on('submit.hook', function (e) {
     e.preventDefault();
 
-    $.post(window.location.pathname + '?do=watchlistForm-submit', '&interval_1=1&interval_2=1&interval_3=1&interval_6=1&remove=true').then(
-    submitForm,
-    function () {
-      window.alert('Something went wrong removing it from "chci videt"');
-      submitForm();
-    });
+    $.post(window.location.pathname + '?do=watchlistForm-submit', '&interval_1=1&interval_2=1&interval_3=1&interval_6=1&remove=true')
+      .then(submit, function () {
+        error();
+        submit();
+      });
   });
 
-  function submitForm() {
+
+  function error() {
+    window.alert('Something went wrong removing it from "chci videt"');
+  }
+
+  function submit() {
     $ratingForm.off('submit.hook').submit();
   }
 })(this, this.jQuery);
